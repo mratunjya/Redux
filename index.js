c89b160acfc66c2d20d0941c9e2bebdcabdc678d
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-03-16 02:54:40 +0530

    Created Redux Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
 
 function buyCake() {
@@ -20,3 +23,16 @@ const reducer = (state = initialState, action) => {
         default: return state;
     }
 }
+
+const store = createStore(reducer); // redux store
+
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state', store.getState());
+});
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
